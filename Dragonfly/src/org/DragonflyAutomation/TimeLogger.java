8bf2e64c8c2f403769ff758a2d03fdce55ac946f
Joseph Perrotta <josperjr@gmail.com>
2021-02-16 17:12:24 -0500

    Changes to TimeLogger
    - working on format of method log details

--- Dragonfly/src/org/DragonflyAutomation/TimeLogger.java
+++ Dragonfly/src/org/DragonflyAutomation/TimeLogger.java
@@ -7,35 +7,41 @@ class TimeLogger {
 	TimeLogger(String classMethodName) {
 		startTime = System.currentTimeMillis();
 		this.classMethodName = classMethodName;
-		//Logger.getInstance().add("==start== " + this.classMethodName + " " + Util.formatDateTime(startTime) + " >");
-		Logger.getInstance().add("==start== " + this.getNameTime());
+		//Logger.getInstance().add("=start=======> " + this.classMethodName + " " + Util.formatDateTime(startTime) + " >");
+		Logger.getInstance().add("=start=======> " + this.getNameTime());
 	}
 
-	private long getDuration() {
-		return System.currentTimeMillis() - startTime;
+	private String getDuration() {
+		//return System.currentTimeMillis() - startTime;
+		//return " <duration = " + Long.toString(System.currentTimeMillis() - startTime) + " milliseconds> ";
+		return " <duration = " + String.format("%,d", (System.currentTimeMillis() - startTime)) + " ms> ";
+		//String.format("%,d", value)
 	}
 
 	private String getNameTime() {
-		return this.classMethodName + " " + Util.formatDateTime(startTime) + " > ";
+		return this.classMethodName + " " + Util.formatDateTime(startTime);
 	}
 
 	void stamp(String textToDisplay) {
-		//Logger.getInstance().add("==stamp==>" + Util.getDateTimestamp() + " " + classMethodName + ": " + textToDisplay);
-		Logger.getInstance().add("==stamp== " + this.getNameTime() + textToDisplay);
+		//Logger.getInstance().add("=stamp=======>" + Util.getDateTimestamp() + " " + classMethodName + ": " + textToDisplay);
+		Logger.getInstance().add("=stamp=======> " + this.getNameTime() + " <> " + textToDisplay);
 	}
 
 	void stop(String textToDisplay) {
-		//Logger.getInstance().add("==stop==>" + classMethodName + ": " + textToDisplay + ": duration = " + this.getDuration() + " milliseconds");
-		Logger.getInstance().add("==stop== " + this.getNameTime() + textToDisplay + " duration = " + this.getDuration() + " milliseconds");
+		//Logger.getInstance().add("=stop========>" + classMethodName + ": " + textToDisplay + ": duration = " + this.getDuration() + " milliseconds");
+		//Logger.getInstance().add("=stop========>" + this.getNameTime() + textToDisplay + " duration = " + this.getDuration() + " milliseconds");
+		Logger.getInstance().add("=stop========> " + this.getNameTime() + this.getDuration() + textToDisplay);
 	}
 
 	void stopCatch(String textToDisplay) {
 		//Logger.getInstance().add("==stopCatch==>" + classMethodName + ": " + textToDisplay + ": duration = " + this.getDuration() + " milliseconds");
-		Logger.getInstance().add("==stopCatch== " + this.getNameTime() + textToDisplay + " duration = " + this.getDuration() + " milliseconds");
+		//Logger.getInstance().add("=stopCatch===>" + this.getNameTime() + textToDisplay + " duration = " + this.getDuration() + " milliseconds");
+		Logger.getInstance().add("=stopCatch===> " + this.getNameTime() + this.getDuration() + textToDisplay);
 	}
 
 	void stopFinally(String textToDisplay) {
 		//Logger.getInstance().add("==stopFinally==>" + classMethodName + ": " + textToDisplay + ": duration = " + this.getDuration() + " milliseconds");
-		Logger.getInstance().add("==stopFinally== " + this.getNameTime() + textToDisplay + " " + ": duration = " + this.getDuration() + " milliseconds");
+		//Logger.getInstance().add("=stopFinally=>" + this.getNameTime() + textToDisplay + " " + ": duration = " + this.getDuration() + " milliseconds");
+		Logger.getInstance().add("=stopFinally=> " + this.getNameTime() + this.getDuration() + textToDisplay);
 	}
 }
