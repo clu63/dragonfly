6f3a4595881b49649786b301770406b8b4f41a4c
clu63 <josperjr@gmail.com>
2021-08-20 18:22:51 -0400

    tested solutions OnMouse and DragAndDrop
    - OnMouse working with Actions solution was to moveoffset element before entering
    - tested on edge, IE, Chrome and Firefox
    - drop is work in progress, javascript function returning error.  Actions is working with IE but not other browsers.

--- Dragonfly/src/org/DragonflyAutomation/ElementDropSync.java
+++ Dragonfly/src/org/DragonflyAutomation/ElementDropSync.java
@@ -1,8 +1,10 @@
 package org.DragonflyAutomation;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
@@ -37,17 +39,59 @@ class ElementDropSync {
 					new ElementEnabled();
 					blnEnabled = true;
 				}
-				ElementDrop.getInstance().elementDrop = Element.getInstance().element;
-				//Actions actions = new Actions(BrowserDriver.getInstance().browserDriver);
-				//actions.dragAndDrop(gobjWebElementDrag, gobjWebElementDrop).perform();
-				// 	Actions action = new Actions(BrowserDriver.getInstance().browserDriver);
-				// 	Action dragDrop = action.dragAndDrop(gobjWebElementDrag, gobjWebElementDrop).build();
-				// 	dragDrop.perform();
-				Actions objActions = new Actions(BrowserDriver.getInstance().browserDriver);
-				Action objDragAndDrop = objActions.clickAndHold(ElementDrag.getInstance().elementDrag).moveToElement(ElementDrop.getInstance().elementDrop).release(ElementDrop.getInstance().elementDrop).build();
-				objDragAndDrop.perform();
-				// 	Actions action = new Actions(BrowserDriver.getInstance().browserDriver);
-				// 	action.dragAndDrop(gobjWebElementDrag, gobjWebElementDrop).build().perform();
+				Actions actions = new Actions(BrowserDriver.getInstance().browserDriver);
+				actions.moveToElement(Element.getInstance().element).perform();
+				
+				//				function createEvent(typeOfEvent) {
+				//				    var event = document.createEvent("CustomEvent");
+				//				    event.initCustomEvent(typeOfEvent, true, true, null);
+				//				    event.dataTransfer = {
+				//				        data: {},
+				//				        setData: function (key, value) {
+				//				            this.data[key] = value;
+				//				        },
+				//				        getData: function (key) {
+				//				            return this.data[key];
+				//				        }
+				//				    };
+				//				    return event;
+				//				}
+				//				function dispatchEvent(element, event, transferData) {
+				//				    if (transferData !== undefined) {
+				//				        event.dataTransfer = transferData;
+				//				    }
+				//				    if (element.dispatchEvent) {
+				//				        element.dispatchEvent(event);
+				//				    } else if (element.fireEvent) {
+				//				        element.fireEvent("on" + event.type, event);
+				//				    }
+				//				}
+				//				function simulateHTML5DragAndDrop(element, target) {
+				//				    var dragStartEvent = createEvent('dragstart');
+				//				    dispatchEvent(element, dragStartEvent);
+				//				    var dropEvent = createEvent('drop');
+				//				    dispatchEvent(target, dropEvent, dragStartEvent.dataTransfer);
+				//				    var dragEndEvent = createEvent('dragend');
+				//				    dispatchEvent(element, dragEndEvent, dropEvent.dataTransfer);
+				//				}
+//				try {
+//					//simulateDragAndDrop(ElementDrag.getInstance().elementDrag, ElementDrop.getInstance().elementDrop);
+//					simulateDragAndDrop();
+//				} catch (Exception e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+				//				ElementDrop.getInstance().elementDrop = Element.getInstance().element;
+				//				//Actions actions = new Actions(BrowserDriver.getInstance().browserDriver);
+				//				//actions.dragAndDrop(gobjWebElementDrag, gobjWebElementDrop).perform();
+				//				// 	Actions action = new Actions(BrowserDriver.getInstance().browserDriver);
+				//				// 	Action dragDrop = action.dragAndDrop(gobjWebElementDrag, gobjWebElementDrop).build();
+				//				// 	dragDrop.perform();
+				//				Actions objActions = new Actions(BrowserDriver.getInstance().browserDriver);
+				//				Action objDragAndDrop = objActions.clickAndHold(ElementDrag.getInstance().elementDrag).moveToElement(ElementDrop.getInstance().elementDrop).release(ElementDrop.getInstance().elementDrop).build();
+				//				objDragAndDrop.perform();
+				//				// 	Actions action = new Actions(BrowserDriver.getInstance().browserDriver);
+				//				// 	action.dragAndDrop(gobjWebElementDrag, gobjWebElementDrop).build().perform();
 				new SleepMilliseconds(10000);
 				blnStatus = true;
 			} catch (NoSuchWindowException | StaleElementReferenceException | NoSuchElementException | NullPointerException | ExceptionElementNotFound | ExceptionMultipleElementsFound e) {
@@ -66,5 +110,57 @@ class ElementDropSync {
 				}
 			}
 		}
+	}
+
+	private void simulateDragAndDrop() throws Exception {
+		//private void simulateDragAndDrop(WebElement elementToDrag, WebElement target) throws Exception {
+		// WebDriver driver = getDriver();
+		JavascriptExecutor javascriptExecutor = (JavascriptExecutor) BrowserDriver.getInstance().browserDriver;
+		//@formatter:off
+		String javaScript = 
+		  "function createEvent(typeOfEvent) {\n" 
+		+ "var event = document.createEvent(\"CustomEvent\");\n" 
+		+ "event.initCustomEvent(typeOfEvent, true, true, null);\n" 
+		+ "   event.dataTransfer = {\n" 
+		+ "       data: {},\n" 
+		+ "       setData: function (key, value) {\n" 
+		+ "           this.data[key] = value;\n" 
+		+ "       },\n" 
+		+ "       getData: function (key) {\n" 
+		+ "           return this.data[key];\n" 
+		+ "       }\n" 
+		+ "   };\n" 
+		+ "   return event;\n" 
+		+ "}\n" 
+		+ "\n" 
+		+ "function dispatchEvent(element, event, transferData) {\n" 
+		+ "   if (transferData !== undefined) {\n" 
+		+ "       event.dataTransfer = transferData;\n" 
+		+ "   }\n" 
+		+ "   if (element.dispatchEvent) {\n" 
+		+ "       element.dispatchEvent(event);\n" 
+		+ "   } else if (element.fireEvent) {\n" 
+		+ "       element.fireEvent(\"on\" + event.type, event);\n" 
+		+ "   }\n" 
+		+ "}\n" 
+		+ "\n" 
+		+ "function simulateHTML5DragAndDrop(element, target) {\n" 
+		+ "   var dragStartEvent = createEvent('dragstart');\n" 
+		+ "   dispatchEvent(element, dragStartEvent);\n" 
+		+ "   var dropEvent = createEvent('drop');\n" 
+		+ "   dispatchEvent(target, dropEvent, dragStartEvent.dataTransfer);\n" 
+		+ "   var dragEndEvent = createEvent('dragend');\n" 
+		+ "   dispatchEvent(element, dragEndEvent, dropEvent.dataTransfer);\n" 
+		+ "}\n" 
+		+ "\n" 
+		
+
+		//+ "var elementToDrag = arguments[0];\n"
+		//+ "var target = arguments[1];\n"
+		//+ "simulateHTML5DragAndDrop(elementToDrag, target);";
+		+ "simulateHTML5DragAndDrop(arguments[0], arguments[1]);";
+		//@formatter:on
+		System.out.println(javaScript);
+		javascriptExecutor.executeScript(javaScript, ElementDrag.getInstance().elementDrag, ElementDrop.getInstance().elementDrop);
 	}
 }
--- Dragonfly/src/org/DragonflyAutomation/ElementOnMouseOver.java
+++ Dragonfly/src/org/DragonflyAutomation/ElementOnMouseOver.java
@@ -1,15 +1,61 @@
 package org.DragonflyAutomation;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.internal.Locatable;
 
 class ElementOnMouseOver {
 	ElementOnMouseOver() {
 		Logger.getInstance().add("  ==start==>ElementOnMouseOver " + Util.getDateTimestamp());
-		Actions objActions = new Actions(BrowserDriver.getInstance().browserDriver);
-		objActions.moveToElement(Element.getInstance().element).build().perform();
-		JavascriptExecutor objJavascriptExecutor = null;
-		objJavascriptExecutor = (JavascriptExecutor) BrowserDriver.getInstance().browserDriver;
-		objJavascriptExecutor.executeScript("arguments[0].onmouseover();", Element.getInstance().element);
+		Actions actions = new Actions(BrowserDriver.getInstance().browserDriver);
+		//		Locatable elementLocation = (Locatable) Element.getInstance().element;
+		//		
+		//		System.out.println(Element.getInstance().element.getLocation());
+		//		System.out.println(Element.getInstance().element.getRect());
+		//		System.out.println(Element.getInstance().element.getSize());
+		//		
+		//		int widthSize = Element.getInstance().element.getSize().getWidth();
+		//		int heightSize = Element.getInstance().element.getSize().getHeight();
+		//		actions.moveByOffset(widthSize / 2 + 1, heightSize / 2 + 1);
+		actions.moveToElement(Element.getInstance().element, -1, -1);
+		//		//actions.moveByOffset(widthSize/2 +1, heightSize/2+1).perform();;
+		//		actions.moveToElement(Element.getInstance().element).perform();
+		//		new SleepMilliseconds(5000);
+		//		actions.moveToElement(Element.getInstance().element, widthSize, 0).perform();
+		//		new SleepMilliseconds(5000);
+		//		actions.moveToElement(Element.getInstance().element, widthSize*2, 0).perform();
+		//		new SleepMilliseconds(5000);
+		//		actions.moveToElement(Element.getInstance().element, widthSize*3, 0).perform();
+		//		new SleepMilliseconds(5000);
+		//		
+		//		try {
+		//			Integer inViewPortX = elementLocation.getCoordinates().inViewPort().getX();
+		//			Integer inViewPortY = elementLocation.getCoordinates().inViewPort().getY();
+		//			System.out.println("<<<<<<<<<<<<<<<<<<inViewPortX = " + inViewPortX + " inViewPortY = " + inViewPortY);
+		//		} catch (Exception e2) {
+		//			System.out.println("XXXXXXXXXXXXXXXXXXinViewPort");
+		//			e2.printStackTrace();
+		//		}
+		//		try {
+		//			Integer onScreenX = elementLocation.getCoordinates().onScreen().getX();
+		//			Integer onScreenY = elementLocation.getCoordinates().onScreen().getY();
+		//			System.out.println("<<<<<<<<<<<<<<<<<<onScreenX = " + onScreenX + " onScreenY = " + onScreenY);
+		//		} catch (Exception e1) {
+		//			System.out.println("XXXXXXXXXXXXXXXXXXonScreen");
+		//			e1.printStackTrace();
+		//		}
+		//		try {
+		//			Integer onPageX = elementLocation.getCoordinates().onPage().getX();
+		//			Integer onPageY = elementLocation.getCoordinates().onPage().getY();
+		//			System.out.println("<<<<<<<<<<<<<<<<<<onPageX = " + onPageX + " onPageY = " + onPageY);
+		//		} catch (Exception e) {
+		//			System.out.println("XXXXXXXXXXXXXXXXXXonPage");
+		//			e.printStackTrace();
+		//		}
+		//		//actions.moveByOffset(0, 0);
+		actions.moveToElement(Element.getInstance().element).perform();
+		//		JavascriptExecutor javascriptExecutor = (JavascriptExecutor) BrowserDriver.getInstance().browserDriver;
+		//		javascriptExecutor.executeScript("arguments[0].onmouseover();", Element.getInstance().element);
 	}
 }
